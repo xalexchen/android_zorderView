315a9407486a9671cf4a91def5c10d017a0ee235
Alex <alex@Miracle>
2013-07-11 14:11:06 +0800

    use TreeMap natural order to implement z order

--- src/com/example/zorder/MainActivity.java
+++ src/com/example/zorder/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zorder;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 import android.app.Activity;
 import android.graphics.Color;
@@ -13,12 +15,10 @@ import android.widget.TextView;
 @SuppressWarnings("deprecation")
 class ZView {
 	private View child;
-	private LayoutParams params;
-	private int z;
-	public void addView (View child,LayoutParams params,int z) {
+	private AbsoluteLayout.LayoutParams params;
+	public ZView (View child,AbsoluteLayout.LayoutParams params) {
 		this.child = child;
 		this.params = params;
-		this.z = z;
 	}
 	public View getView() {
 		return child;
@@ -26,20 +26,16 @@ class ZView {
 	public LayoutParams getParams() {
 		return params;
 	}
-	public int getZ() {
-		return z;
-	}
 }
 
 public class MainActivity extends Activity {
-	private ArrayList<ZView> zview;
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		zview = new ArrayList<ZView>();
 		AbsoluteLayout layout = new AbsoluteLayout(this);
-		
+		Map<Integer,ZView> zMap = new TreeMap<Integer,ZView>();
+
 		
 		TextView z1 = new TextView(this);
 		z1.setBackgroundColor(Color.parseColor("red"));
@@ -48,10 +44,8 @@ public class MainActivity extends Activity {
 				500,
 				300,
 				300);
-		ZView z1View = new ZView();
-		z1View.addView(z1, lpz1,1);
-		zview.add(z1View);
-		
+		ZView z1View = new ZView(z1, lpz1);
+		zMap.put(2, z1View);
 		
 		TextView z2 = new TextView(this);
 		z2.setBackgroundColor(Color.parseColor("green"));
@@ -60,10 +54,8 @@ public class MainActivity extends Activity {
 				300,
 				400,
 				100);
-		ZView z2View = new ZView();
-		z2View.addView(z2, lpz2,2);
-		zview.add(z2View);
-		
+		ZView z2View = new ZView(z2, lpz2);
+		zMap.put(5, z2View);
 		
 		TextView z0 = new TextView(this);
 		z0.setBackgroundColor(Color.parseColor("blue"));
@@ -72,19 +64,12 @@ public class MainActivity extends Activity {
 				500,
 				0,
 				0);
-		ZView z0View = new ZView();
-		z0View.addView(z0, lpz0,0);
-		zview.add(z0View);
+		ZView z0View = new ZView(z0, lpz0);
+		zMap.put(9, z0View);
 		
-		// according to z order ,add into viewgroup
-		for(int z = 0; z < zview.size();z++) {
-			for (ZView view:zview) {
-				if (z == view.getZ()) {
-					layout.addView(view.getView(), view.getParams());
-				}
-			}
+		for (Integer s :zMap.keySet()){
+			layout.addView(zMap.get(s).getView(), zMap.get(s).getParams());
 		}
-		zview = null;
 		setContentView(layout);
 	}
 }
